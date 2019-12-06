7d8752c5892630b4e71bdb8702f903e6d526eae6
Ibukunoluwa Adeeko <ibukunadeeko@gmail.com>
2019-12-06 08:15:17 +0100

    Lesson 2 complete

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,23 @@
-export default {}
+export const createOneItem = (req, res) => {
+  res.send({ message: 'Item Created' })
+}
+export const getAllItems = (req, res) => {
+  res.send({ message: 'Get All Items' })
+}
+export const getOneItem = (req, res) => {
+  res.send({ message: `Got One Item with id: '${req.params.id}'` })
+}
+export const updateOneItem = (req, res) => {
+  res.send({ message: `Item with id '${req.params.id}' Updated` })
+}
+export const deleteOneItem = (req, res) => {
+  res.send({ message: `Item with id '${req.params.id}' Deleted` })
+}
+
+export default {
+  createOneItem,
+  getAllItems,
+  getOneItem,
+  updateOneItem,
+  deleteOneItem
+}
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,23 @@
 import { Router } from 'express'
+import {
+  getAllItems,
+  getOneItem,
+  createOneItem,
+  updateOneItem,
+  deleteOneItem
+} from './item.controllers'
 
 const router = Router()
+
+router
+  .route('/')
+  .get(getAllItems)
+  .post(createOneItem)
+
+router
+  .route('/:id')
+  .get(getOneItem)
+  .put(updateOneItem)
+  .delete(deleteOneItem)
 
 export default router
