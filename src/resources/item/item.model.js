04c87c0eab7ddf43e162c71d28991a5351717986
Ibukunoluwa Adeeko <ibukunadeeko@gmail.com>
2019-12-06 15:12:08 +0100

    add item model

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,40 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['active', 'complete', 'pastdue'],
+      default: 'active'
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'user',
+      required: true
+    },
+    list: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'list',
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+/**
+ * What this does is that you are telling mongodb that for every list, every name should be unique.
+ * So if you have 'wash clothes' in list 1 and you try to create another 'wash clothes' for list 1 it would throw an error
+ * To fix this you either change the name or you assign 'wash clothes' to another list
+ */
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
+
 export const Item = mongoose.model('item', itemSchema)
